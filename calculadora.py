95657b8890937114a7198c9cc92ad98fb7415b46
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-25 21:47:04 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,13 @@ def calcular():
     num1 = float(entry_num1.get())
     num2 = float(entry_num2.get())
     operacion = operacion_var.get()
+    if operacion == "Suma":
+        resultado = num1 + num2
+    elif operacion == "Resta":
+        resultado = num1 - num2
+    else:
+        resultado = "Operación no válida"
+        
+    label_resultado.config(text="Resultado: " + str(resultado))    
 
     
