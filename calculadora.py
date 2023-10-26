f76844aec2ff0f55989936172a6a0d84df9526e1
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-25 21:45:51 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,4 +2,6 @@ import tkinter as tk
 
 def calcular():
     """En esta funcion estará todo lo relacionado con la calculadora, luego se puede refactorizar"""
+    num1 = float(entry_num1.get())
+
     
