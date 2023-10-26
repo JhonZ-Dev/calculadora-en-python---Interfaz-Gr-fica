a3eda7decb3ed3f3f6c5f145af5d94e3b2a75310
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-25 21:46:25 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -3,5 +3,7 @@ import tkinter as tk
 def calcular():
     """En esta funcion estará todo lo relacionado con la calculadora, luego se puede refactorizar"""
     num1 = float(entry_num1.get())
+    num2 = float(entry_num2.get())
+    operacion = operacion_var.get()
 
     
